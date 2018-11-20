fc3a8650b6d02f30b78c9c9a4a76c7d391b51f82
Gabrielle Hugo <gabrielle.hugo83@gmail.com>
2018-11-20 17:14:03 +0100

    Test telescope navigation school: evth ok

--- RecoTracker/TkNavigation/test/NavigationSchoolAnalyzer_cfg.py
+++ RecoTracker/TkNavigation/test/NavigationSchoolAnalyzer_cfg.py
@@ -7,13 +7,19 @@ process = cms.Process("NavigationSchoolAnalyze")
 # process.load("Configuration.StandardSequences.Geometry_cff")
 #process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
 #process.load('Configuration.Geometry.GeometryExtended2023D1Reco_cff')
-process.load('Configuration.Geometry.GeometryExtended2023D4Reco_cff')
-process.load('Configuration.StandardSequences.MagneticField_AutoFromDBCurrent_cff')
+#process.load('Configuration.Geometry.GeometryExtended2023D4Reco_cff')
+process.load('Geometry.PixelTelescope.PixelTelescopeRecoGeometry_cfi')
+process.load('Configuration.StandardSequences.MagneticField_0T_cff')
+
+#process.load('Configuration.StandardSequences.MagneticField_AutoFromDBCurrent_cff')
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+#from Configuration.AlCa.GlobalTag import GlobalTag
+#process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:phase2_realistic', '')
 from Configuration.AlCa.GlobalTag import GlobalTag
-process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:phase2_realistic', '')
+process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:phase1_2017_realistic', '')
 # process.load("Configuration.StandardSequences.MagneticField_cff")
-process.load("RecoTracker.TkNavigation.NavigationSchoolESProducer_cff")
+#process.load("RecoTracker.TkNavigation.NavigationSchoolESProducer_cff")
+process.load('RecoTracker.TkNavigation.TelescopeNavigationSchoolESProducer_cfi')
 
 #process.MessageLogger = cms.Service("MessageLogger",
 #    destinations = cms.untracked.vstring('detailedInfo')
@@ -31,12 +37,12 @@ process.source = cms.Source("EmptySource")
 process.navigationSchoolAnalyzer = cms.EDAnalyzer("NavigationSchoolAnalyzer",
 #    navigationSchoolName = cms.string('BeamHaloNavigationSchool')
 #    navigationSchoolName = cms.string('CosmicNavigationSchool')
-    navigationSchoolName = cms.string('SimpleNavigationSchool')
+    navigationSchoolName = cms.string('TelescopeNavigationSchool')
 )
 
-process.muonNavigationTest = cms.EDAnalyzer("MuonNavigationTest")
+#process.muonNavigationTest = cms.EDAnalyzer("MuonNavigationTest")
 
 
-process.p = cms.Path(process.navigationSchoolAnalyzer+process.muonNavigationTest)
+process.p = cms.Path(process.navigationSchoolAnalyzer)
 
 
