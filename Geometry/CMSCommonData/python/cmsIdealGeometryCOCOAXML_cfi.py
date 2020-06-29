8544652559d213cf7c2ef480031e16d05aec45ae
Gabrielle Hugo <gabrielle.hugo83@gmail.com>
2020-06-29 10:34:59 +0200

    Forgot to include IdealGeometry record cfi file

---
+++ Geometry/CMSCommonData/python/cmsIdealGeometryCOCOAXML_cfi.py
@@ -0,0 +1,19 @@
+import FWCore.ParameterSet.Config as cms
+
+#XMLIdealGeometryESSource = cms.ESSource("XMLIdealGeometryESSource",
+#    geomXMLFiles = cms.vstring(
+#        'Geometry/ForwardSimData/data/ForwardShieldProdCuts.xml',
+#        'Geometry/CMSCommonData/data/FieldParameters.xml'),
+#    rootNodeName = cms.string('cms:OCMS')
+#)
+
+XMLIdealGeometryESSource = cms.ESSource("XMLIdealGeometryESSource",
+    #rootNodeName = cms.string('cms:OCMS'),
+    rootNodeName = cms.string('table2DWithMirror:MotherOfAllBoxes'),
+    geomXMLFiles = cms.vstring(
+        'Geometry/CMSCommonData/data/materials.xml',
+        'Alignment/CocoaApplication/test/table2DWithMirror.xml',
+        #'Geometry/CMSCommonData/data/cms.xml'
+        )    
+)
+
