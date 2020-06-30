c4a132792a6263a8ef383ffc440f1048860c0dc2
Gabrielle Hugo <gabrielle.hugo83@gmail.com>
2020-06-30 12:44:50 +0200

    Independant from port to dd4hep: Try to recreate connection to db file to get OpticalAlignmentsRcd...

--- Alignment/CocoaApplication/test/cocoaAnalyzer_cfg.py
+++ Alignment/CocoaApplication/test/cocoaAnalyzer_cfg.py
@@ -18,33 +18,17 @@ process.DDCompactViewESProducer = cms.ESProducer("DDCompactViewESProducer",
                                                 )
                                                    
                          
-#process.load("CondCore.CondDB.CondDB_cfi")
-#process.CondDB.connect = 'sqlite_file:/home/ghugo/CMSSW/CMSSW_11_2_0_pre1/src/Alignment/CocoaApplication/test/OpticalAlignments.db'
-#process.PoolDBESSource = cms.ESSource("PoolDBESSource",
-#    process.CondDB,
-#    DumpStat=cms.untracked.bool(True),
-#    toGet = cms.VPSet(cms.PSet(
-#        record = cms.string('IdealGeometryRecord'),
-#        tag = cms.string("hey")
-#    )),
-#)
-
-
-# Reading from DB
-#process.load("CondCore.DBCommon.CondDBSetup_cfi")
-#process.PoolDBESSource = cms.ESSource("PoolDBESSource",
-#    process.CondDBSetup,
-#    toGet = cms.VPSet(
-#       cms.PSet(
-#            record = cms.string('OpticalAlignmentsRcd'),
-#            tag = cms.string('OPTICALALIGNMENTS')
-#        )),
-#    connect = cms.string('sqlite_file:/home/ghugo/CMSSW/CMSSW_11_2_0_pre1/src/Alignment/CocoaApplication/test/OpticalAlignments.db')
-#)
-                            
-                        
-                                                 
-
+process.load("CondCore.CondDB.CondDB_cfi")
+process.CondDB.connect = 'sqlite_file:/home/ghugo/CMSSW/CMSSW_11_2_0_pre1/src/Alignment/CocoaApplication/test/OpticalAlignments.db'
+process.PoolDBESSource = cms.ESSource("PoolDBESSource",
+    process.CondDB,
+    DumpStat=cms.untracked.bool(True),
+    toGet = cms.VPSet(cms.PSet(
+        record = cms.string('OpticalAlignmentsRcd'),
+        tag = cms.string("OpticalAlignmentsRcd")
+    )),
+)
+                                                
 process.cocoa = cms.EDAnalyzer('CocoaAnalyzer',
 				maxEvents = cms.int32(1),
 				cocoaDaqRootFile = cms.string("cocoaDaqTest.root"),
