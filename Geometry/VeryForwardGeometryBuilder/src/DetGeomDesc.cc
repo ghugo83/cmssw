92a868cbc1923da79993bf043707440499f9a735
Gabrielle Hugo <gabrielle.hugo83@gmail.com>
2020-07-07 17:40:19 +0200

    Port PPSGeometryBuilder to dd4hep + Fixes in XMLs.

--- Geometry/VeryForwardGeometryBuilder/src/DetGeomDesc.cc
+++ Geometry/VeryForwardGeometryBuilder/src/DetGeomDesc.cc
@@ -15,25 +15,241 @@
 #include "DetectorDescription/Core/interface/DDFilteredView.h"
 #include "DetectorDescription/Core/interface/DDSolid.h"
 
+#include "DataFormats/Math/interface/CMSUnits.h"
+#include "DetectorDescription/DDCMS/interface/DDFilteredView.h"
+#include "DetectorDescription/DDCMS/interface/DDShapes.h"
+#include "DetectorDescription/DDCMS/interface/DDSolidShapes.h"
+#include "TGeoMatrix.h"
+
+#include "DataFormats/CTPPSDetId/interface/TotemRPDetId.h"
+#include "DataFormats/CTPPSDetId/interface/TotemTimingDetId.h"
+#include "DataFormats/CTPPSDetId/interface/CTPPSPixelDetId.h"
+#include "DataFormats/CTPPSDetId/interface/CTPPSDiamondDetId.h"
+#include "Geometry/VeryForwardGeometryBuilder/interface/CTPPSDDDNames.h"
+
+#include "CondFormats/GeometryObjects/interface/PDetGeomDesc.h"
+
 //#include "DataFormats/CTPPSAlignment/interface/RPAlignmentCorrectionData.h"
 #include "CondFormats/PPSObjects/interface/CTPPSRPAlignmentCorrectionData.h"
 
 using namespace std;
 
+using namespace cms_units::operators;
+
 //----------------------------------------------------------------------------------------------------
 
 DetGeomDesc::DetGeomDesc(DDFilteredView* fv)
-    : m_trans(fv->translation()),
-      m_rot(fv->rotation()),
-      m_name(((fv->logicalPart()).ddname()).name()),
-      m_params(((fv->logicalPart()).solid()).parameters()),
-      m_copy(fv->copyno()),
-      m_z(fv->geoHistory().back().absTranslation().z()),
-      m_sensorType("") {
+  : m_trans(fv->translation()),
+    m_rot(fv->rotation()),
+    m_name(((fv->logicalPart()).ddname()).name()),
+    m_params(((fv->logicalPart()).solid()).parameters()),
+    m_copy(fv->copyno()),
+    m_z(fv->geoHistory().back().absTranslation().z()),
+    m_sensorType("") {
   std::string sensor_name = fv->geoHistory().back().logicalPart().name().fullname();
   std::size_t found = sensor_name.find(DDD_CTPPS_PIXELS_SENSOR_NAME);
   if (found != std::string::npos && sensor_name.substr(found - 4, 3) == DDD_CTPPS_PIXELS_SENSOR_TYPE_2x2) {
     m_sensorType = DDD_CTPPS_PIXELS_SENSOR_TYPE_2x2;
+  }
+}
+
+//----------------------------------------------------------------------------------------------------
+
+// Constructor from DD4Hep DDFilteredView
+
+DetGeomDesc::DetGeomDesc(cms::DDFilteredView* fv, const cms::DDSpecParRegistry& allSpecParSections)
+  /*: m_trans(fv->translation()),
+    m_rot(fv->rotation()),
+    m_name(fv->name()),
+    //      m_params(((fv->logicalPart()).solid()).parameters()),
+    m_copy(fv->copyNum()),
+    m_z((fv->geoHistory().front()->GetMatrix()->GetTranslation())[2]),
+    m_sensorType("") {
+    //  std::string sensor_name = fv->geoHistory().back().logicalPart().name().fullname();
+    /*
+    std::string sensor_name = fv->history().back().logicalPart().name().fullname();
+    std::size_t found = sensor_name.find(DDD_CTPPS_PIXELS_SENSOR_NAME);
+    if (found != std::string::npos && sensor_name.substr(found - 4, 3) == DDD_CTPPS_PIXELS_SENSOR_TYPE_2x2) {
+    m_sensorType = DDD_CTPPS_PIXELS_SENSOR_TYPE_2x2;
+    }
+  */
+
+
+
+  : m_trans(fv->translation() / 1._mm),  // Convert cm (DD4hep) to mm (legacy)
+    m_rot(fv->rotation()),
+    m_name(fv->name()),
+    //m_params = ((fv->volume()).solid()).parameters(); TO DOOOOOOOOOOOOOOOOOOOOOOOOO
+    //m_params(fv->parameters()),
+    
+
+    m_geographicalID(computeDetID(fv)),
+    m_copy(fv->copyNum()),
+    //m_z = fv->geoHistory().back().absTranslation().z();
+    m_z(fv->translation().z() / 1._mm)  // Convert cm (DD4hep) to mm (legacy)
+    //m_sensorType(fv->path()) 
+{
+
+
+  const std::string parameterName = "2x2RPixWafer";
+  cms::DDSpecParRefs filteredSpecParSections;
+  allSpecParSections.filter(filteredSpecParSections, parameterName);
+  for (const auto& mySpecParSection : filteredSpecParSections) {
+    if (mySpecParSection->hasPath(fv->path())) {
+      //return mySpecParSection->value<std::vector<T>>(parameterName);
+      m_sensorType = DDD_CTPPS_PIXELS_SENSOR_TYPE_2x2;
+    }
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+  const cms::DDSolidShape& mySolidShape = cms::dd::getCurrentShape(*fv);
+  std::cout << "m_name = " << m_name << std::endl;
+
+  if (mySolidShape == cms::DDSolidShape::ddbox) {
+    const cms::dd::DDBox& myShape = cms::dd::DDBox(*fv);
+    m_params = { myShape.halfX() / 1._mm,
+		 myShape.halfY() / 1._mm,
+		 myShape.halfZ() / 1._mm
+    }; 
+  }
+
+  else if (mySolidShape == cms::DDSolidShape::ddcons) {
+    const cms::dd::DDCons& myShape = cms::dd::DDCons(*fv);
+    m_params = { myShape.zhalf() / 1._mm,
+		 myShape.rInMinusZ() / 1._mm,
+		 myShape.rOutMinusZ() / 1._mm,
+		 myShape.rInPlusZ() / 1._mm,
+		 myShape.rOutPlusZ() / 1._mm,
+		 myShape.phiFrom(),
+		 myShape.deltaPhi()
+    }; 
+  }
+  else if (mySolidShape == cms::DDSolidShape::ddtrap) {
+    const cms::dd::DDTrap& myShape = cms::dd::DDTrap(*fv);
+    m_params = { myShape.halfZ() / 1._mm,
+		 myShape.theta(),
+		 myShape.phi(),
+		 myShape.y1() / 1._mm,
+		 myShape.x1() / 1._mm,
+		 myShape.x2() / 1._mm,
+		 myShape.alpha1(),
+		 myShape.y2() / 1._mm,
+		 myShape.x3() / 1._mm,
+		 myShape.x4() / 1._mm,		 
+		 myShape.alpha2()
+    }; 
+  }
+  else if (mySolidShape == cms::DDSolidShape::ddtubs) {
+    const cms::dd::DDTubs& myShape = cms::dd::DDTubs(*fv);
+    m_params = { myShape.zhalf() / 1._mm,
+		 myShape.rIn() / 1._mm,
+		 myShape.rOut() / 1._mm,
+		 myShape.startPhi(),
+		 myShape.deltaPhi()
+    };
+  }
+  else if (mySolidShape == cms::DDSolidShape::ddtrunctubs) {
+    const cms::dd::DDTruncTubs& myShape = cms::dd::DDTruncTubs(*fv);
+    m_params = { myShape.zHalf() / 1._mm,
+		 myShape.rIn() / 1._mm,
+		 myShape.rOut() / 1._mm,
+		 myShape.startPhi(),
+		 myShape.deltaPhi(),
+		 myShape.cutAtStart() / 1._mm,
+		 myShape.cutAtDelta() / 1._mm,
+		 static_cast<double>(myShape.cutInside())
+    }; 
+  }
+  else if (mySolidShape == cms::DDSolidShape::dd_not_init) {
+    auto myShape = fv->solid();
+    const std::vector<double>& params = myShape.dimensions();
+    if (fv->isA<dd4hep::Trd1>()) {
+      m_params = { params[3] / 1._mm, // z
+		   0.,
+		   0.,
+		   params[2] / 1._mm, // y
+		   params[0] / 1._mm, // x1
+		   params[0] / 1._mm, // x1
+		   0.,
+		   params[2] / 1._mm, // y
+		   params[1] / 1._mm, // x2
+		   params[1] / 1._mm, // x2
+		   0.  
+      };
+    }
+    else if (fv->isA<dd4hep::Polycone>()) {
+      int counter = 0;
+      for (const auto& para : params) {	
+	if (counter != 2) {
+	  m_params.emplace_back( para * (counter >= 2 ? (1. / 1._mm) : 1.));
+	}
+	++counter;
+      }
+    }
+    else {
+      //if (fv->solid()->GetShape()->IsA() != TGeoCompositeShape::Class()) {
+      //if (!fv->isA<dd4hep::BooleanSolid>()) {
+      //if (fv->getShapePtr<TGeoCompositeShape::Class()>()
+      std::cout << "DetGeomDesc::DetGeomDesc(cms::DDFilteredView* fv): ERROR: shape not supported for " 
+		<< m_name << ", Id = " << m_geographicalID
+		<< std::endl;
+      //}
+    }
+  }
+
+
+
+
+
+  /*
+    std::cout << "DetGeomDesc::DetGeomDesc m_name = " << m_name << std::endl;
+    std::cout << "view->copyNumbers() = ";
+    for (const auto& num : fv->copyNumbers()) {
+    std::cout << num << " ";
+    }
+    std::cout << " " << std::endl;*/
+
+ 
+  //const std::string sensor_name {fv->name()};
+  //const std::string sensor_name = fv->fullname();
+  //std::string sensor_name = fv->geoHistory().back().logicalPart().name().fullname();
+  /* std::size_t found = sensor_name.find(DDD_CTPPS_PIXELS_SENSOR_NAME);
+     if (found != std::string_view::npos && sensor_name.substr(found - 4, 3) == DDD_CTPPS_PIXELS_SENSOR_TYPE_2x2) {
+     m_sensorType = DDD_CTPPS_PIXELS_SENSOR_TYPE_2x2;
+     }*/
+
+}
+
+//----------------------------------------------------------------------------------------------------
+
+DetGeomDesc::DetGeomDesc(PDetGeomDesc* pd) {
+  for (auto i : pd->container_) {
+    
+    DetGeomDesc* gd = new DetGeomDesc();
+    
+    gd->setTranslation(i.dx_,i.dy_,i.dz_);
+    gd->setRotation(i.axx_,i.axy_,i.axz_,
+                    i.ayx_,i.ayy_,i.ayz_,
+                    i.azx_,i.azy_,i.azz_);
+    gd->setName(i.name_);
+    gd->setParams(i.params_);
+    gd->setGeographicalID(i.geographicalID_);
+    gd->setCopyno(i.copy_);
+    gd->setParentZPosition(i.z_);
+    gd->setSensorType(i.sensorType_);
+    
+    this->addComponent(gd);
   }
 }
 
@@ -85,4 +301,115 @@ void DetGeomDesc::deepDeleteComponents() {
 void DetGeomDesc::applyAlignment(const CTPPSRPAlignmentCorrectionData& t) {
   m_rot = t.getRotationMatrix() * m_rot;
   m_trans = t.getTranslation() + m_trans;
+}
+
+
+DetId DetGeomDesc::computeDetID(cms::DDFilteredView* fv) {
+  DetId geoID;
+
+
+  std::string name(fv->name());
+
+  // strip sensors
+  if (name == DDD_TOTEM_RP_SENSOR_NAME) {
+    const std::vector<int>& copy_num = fv->copyNos();
+    // check size of copy numbers array
+    if (copy_num.size() < 4)
+      throw cms::Exception("DDDTotemRPContruction")
+	<< "size of copyNumbers for strip sensor is " << copy_num.size() << ". It must be >= 4.";
+
+    // extract information
+    const unsigned int decRPId = copy_num[2];
+    const unsigned int arm = decRPId / 100;
+    const unsigned int station = (decRPId % 100) / 10;
+    const unsigned int rp = decRPId % 10;
+    const unsigned int detector = copy_num[0];
+    geoID = TotemRPDetId(arm, station, rp, detector);
+  }
+
+  // strip and pixels RPs
+  else if (name == DDD_TOTEM_RP_RP_NAME || name == DDD_CTPPS_PIXELS_RP_NAME) {
+    unsigned int decRPId = fv->copyNum();
+
+    // check if it is a pixel RP
+    if (decRPId >= 10000) {
+      decRPId = decRPId % 10000;
+      const unsigned int armIdx = (decRPId / 100) % 10;
+      const unsigned int stIdx = (decRPId / 10) % 10;
+      const unsigned int rpIdx = decRPId % 10;
+      geoID = CTPPSPixelDetId(armIdx, stIdx, rpIdx);
+    } else {
+      const unsigned int armIdx = (decRPId / 100) % 10;
+      const unsigned int stIdx = (decRPId / 10) % 10;
+      const unsigned int rpIdx = decRPId % 10;
+      geoID = TotemRPDetId(armIdx, stIdx, rpIdx);
+    }
+  }
+
+  else if (std::regex_match(name, std::regex(DDD_TOTEM_TIMING_SENSOR_TMPL))) {
+    const std::vector<int>& copy_num = fv->copyNos();
+    // check size of copy numbers array
+    if (copy_num.size() < 5)
+      throw cms::Exception("DDDTotemRPContruction")
+	<< "size of copyNumbers for TOTEM timing sensor is " << copy_num.size() << ". It must be >= 5.";
+
+    const unsigned int decRPId = copy_num[3];
+    const unsigned int arm = decRPId / 100, station = (decRPId % 100) / 10, rp = decRPId % 10;
+    const unsigned int plane = copy_num[1], channel = copy_num[0];
+    geoID = TotemTimingDetId(arm, station, rp, plane, channel);
+  }
+
+  else if (name == DDD_TOTEM_TIMING_RP_NAME) {
+    const unsigned int arm = fv->copyNum() / 100, station = (fv->copyNum() % 100) / 10, rp = fv->copyNum() % 10;
+    geoID = TotemTimingDetId(arm, station, rp);
+  }
+
+  // pixel sensors
+  else if (name == DDD_CTPPS_PIXELS_SENSOR_NAME) {
+    const std::vector<int>& copy_num = fv->copyNos();
+    // check size of copy numbers array
+    if (copy_num.size() < 5)
+      throw cms::Exception("DDDTotemRPContruction")
+	<< "size of copyNumbers for pixel sensor is " << copy_num.size() << ". It must be >= 5.";
+
+    // extract information
+    const unsigned int decRPId = copy_num[3] % 10000;
+    const unsigned int arm = decRPId / 100;
+    const unsigned int station = (decRPId % 100) / 10;
+    const unsigned int rp = decRPId % 10;
+    const unsigned int detector = copy_num[1] - 1;
+    geoID = CTPPSPixelDetId(arm, station, rp, detector);
+  }
+
+  // diamond/UFSD sensors
+  else if (name == DDD_CTPPS_DIAMONDS_SEGMENT_NAME || name == DDD_CTPPS_UFSD_SEGMENT_NAME) {
+    const std::vector<int>& copy_num = fv->copyNos();
+
+    const unsigned int id = copy_num[0];
+    const unsigned int arm = copy_num[copy_num.size()-3] - 1;
+    const unsigned int station = 1;
+    const unsigned int rp = 6;
+    const unsigned int plane = (id / 100);
+    const unsigned int channel = id % 100;
+
+    geoID = CTPPSDiamondDetId(arm, station, rp, plane, channel);
+  }
+
+  // diamond/UFSD RPs
+  else if (name == DDD_CTPPS_DIAMONDS_RP_NAME) {
+    const std::vector<int>& copy_num = fv->copyNos();
+
+    // check size of copy numbers array
+    if (copy_num.size() < 3)
+      throw cms::Exception("DDDTotemRPContruction")
+	<< "size of copyNumbers for diamond RP is " << copy_num.size() << ". It must be >= 3.";
+
+    const unsigned int arm = copy_num[(copy_num.size()-3)] - 1;
+    const unsigned int station = 1;
+    const unsigned int rp = 6;
+
+    geoID = CTPPSDiamondDetId(arm, station, rp);
+  }
+
+  return geoID;
 }
